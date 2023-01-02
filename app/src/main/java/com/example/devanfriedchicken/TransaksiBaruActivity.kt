bc062f11f9ed37920a1ca5b899736ee58927cefa
waldofx <90760767+waldofx@users.noreply.github.com>
2023-01-02 22:31:18 +0700

    button tambah kurang produk

--- app/src/main/java/com/example/devanfriedchicken/TransaksiBaruActivity.kt
+++ app/src/main/java/com/example/devanfriedchicken/TransaksiBaruActivity.kt
@@ -18,26 +18,83 @@ class TransaksiBaruActivity() : AppCompatActivity() {
         val factory = TransactionViewModelFactory(transactionRepository)
         transactionViewModel = ViewModelProvider(this, factory).get(TransactionViewModel::class.java)
 
-        // Click listener on Save button
-        // to save all data.
+        //init quantities
+        var quantity_pahabawah = 0
+        var quantity_sayap = 0
+
+        // Click listener on Save button to save all data.
         buttonCatat.setOnClickListener {
 
             // Take all three inputs in different variables from user
             // and add it in Transaction Items database
-            val name_pahabawah = "Paha Bawah"
-            val quantity_pahabawah = etPahaBawahQuantity.text.toString().toInt()
-            val price_pahabawah = 7000
-
-//            if (quantity != 0) {
+            if (quantity_pahabawah != 0) {
+                val name_pahabawah = "Paha Bawah"
+                quantity_pahabawah = etPahaBawahQuantity.text.toString().toInt()
+                val price_pahabawah = 7000
                 val item_pahabawah = TransactionItems(name_pahabawah, quantity_pahabawah, price_pahabawah)
-                transactionViewModel.insert(item_pahabawah) //don't know why can't import insert
+                transactionViewModel.insert(item_pahabawah)
                 Toast.makeText(applicationContext, "Item inserted...", Toast.LENGTH_SHORT).show()
-//            }
+            }
+
+            if (quantity_sayap != 0) {
+                val name_sayap = "Sayap"
+                quantity_sayap = etSayapQuantity.text.toString().toInt()
+                val price_sayap = 7000
+                val item_sayap = TransactionItems(name_sayap, quantity_sayap, price_sayap)
+                transactionViewModel.insert(item_sayap) //don't know why can't import insert
+                Toast.makeText(applicationContext, "Item inserted...", Toast.LENGTH_SHORT).show()
+            }
         }
 
-        // On click listener on cancel text to close dialog box
+        // On click listener on batal to clear
         buttonBatal.setOnClickListener {
             etPahaBawahQuantity.setText("")
+            quantity_pahabawah = 0
+            etSayapQuantity.setText("")
+            quantity_sayap = 0
         }
+
+
+        // On click listener on tambah kurang paha bawah
+        tambah_pb.setOnClickListener {
+            if(etPahaBawahQuantity.text.toString() == "") {
+                etPahaBawahQuantity.setText(quantity_pahabawah.toString())
+            } else{
+                quantity_pahabawah++
+                etPahaBawahQuantity.setText(quantity_pahabawah.toString())
+            }
+        }
+
+        kurang_pb.setOnClickListener {
+            if(etPahaBawahQuantity.text.toString() != "0"){
+                if(etPahaBawahQuantity.text.toString() == "") {
+                    etPahaBawahQuantity.setText(quantity_pahabawah.toString())
+                } else{
+                    quantity_pahabawah--
+                    etPahaBawahQuantity.setText(quantity_pahabawah.toString())
+                }
+            }
+        }
+
+        tambah_s.setOnClickListener {
+            if(etSayapQuantity.text.toString() == "") {
+                etSayapQuantity.setText(quantity_sayap.toString())
+            } else{
+                quantity_sayap++
+                etSayapQuantity.setText(quantity_sayap.toString())
+            }
+        }
+
+        kurang_s.setOnClickListener {
+            if(etSayapQuantity.text.toString() != "0"){
+                if(etSayapQuantity.text.toString() == "") {
+                    etSayapQuantity.setText(quantity_sayap.toString())
+                } else{
+                    quantity_sayap--
+                    etSayapQuantity.setText(quantity_sayap.toString())
+                }
+            }
+        }
+
     }
 }
